5f7ae431f97de87bc66839136ddba52932e6b1f7
Chris Offiong <chrisoffiong@gmail.com>
2019-04-06 10:22:45 -0400

    Updates

---
+++ precache-manifest.efe4d32002e1f946742379e58f9518f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "8968577556757882d92f",
+    "url": "/Clicky-Game/static/js/main.00e80c8a.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8968577556757882d92f",
+    "url": "/Clicky-Game/static/css/main.012a2135.chunk.css"
+  },
+  {
+    "revision": "9306a4f3044fec8cfe42595f72b85ee7",
+    "url": "/Clicky-Game/index.html"
+  }
+];
